861c98f1994e848897a1ec41975d7d6aa612214b
ariefbustillo <ariefbustillo@gmail.com>
2021-11-18 17:47:29 -0500

    changes

--- server.js
+++ server.js
@@ -35,14 +35,14 @@ app.post("/app/new/", (req, res) => {
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users/", (req, res) => {	
 	const stmt = db.prepare("SELECT * FROM userinfo").all();
-	res.status(200).json(stmt);
+	res.status(200);
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/user/:id", (req, res) => {	
 	let id = req.params.id;
-	const stmt = db.prepare(`SELECT * FROM userinfo WHERE id = ${id}`).all();
-	res.status(200).json(stmt);
+	const stmt = db.prepare(`SELECT * FROM userinfo WHERE id = ${id}`).get();
+	res.status(200);
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {	
